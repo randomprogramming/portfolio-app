774f8c01344ac6dc38851840a6549c263f1853e3
randomprogramming <isnipedodac@gmail.com>
2019-12-18 10:27:47 +0100

    added a custom font

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export default class Home extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Hommmee</h1>
+			<div className="home-container">
+				<h1 style={{ fontFamily: "Raleway Regular" }}>Hommmee</h1>
 				<div style={{ height: "1000px", width: "100px" }}></div>
 			</div>
 		);
