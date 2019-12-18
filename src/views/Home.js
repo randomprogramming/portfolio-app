fbc1f9a1dcab8bb40ccea505fea8168019bbde8c
randomprogramming <isnipedodac@gmail.com>
2019-12-18 16:44:53 +0100

    added some content to the homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 
+import "../css/Home.css";
+
 export default class Home extends Component {
 	render() {
 		return (
 			<div className="home-container">
-				<h1 style={{ fontFamily: "Raleway Regular" }}>Hommmee</h1>
-				<div style={{ height: "1000px", width: "100px" }}></div>
+				<div className="home-welcome-text-container">
+					<div className="home-welcome-text">
+						<div style={first}>Hello</div>
+						<div>and</div>
+						<div style={third}>Welcome</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
+
+const first = {
+	padding: "0em 1em",
+	marginLeft: "-3em",
+};
+const third = {
+	padding: "0em 1em",
+	marginLeft: "5em",
+};
