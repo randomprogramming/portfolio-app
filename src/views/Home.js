da3dc7eddb609813c5277d386f0592a2a68e61f9
randomprogramming <isnipedodac@gmail.com>
2019-12-18 17:07:42 +0100

    added some text content to the homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -13,6 +13,17 @@ export default class Home extends Component {
 						<div style={third}>Welcome</div>
 					</div>
 				</div>
+				<div className="home-content">
+					<p className="home-content-text">
+						Great, you found my site! I will be using this website to show of my front end skills. I
+						also have experience in back end technologies, but more on that later on the 'About me'
+						page. This page was built solely using the{" "}
+						<a href="https://reactjs.org/">React Framework</a> developed by Facebook. You can see
+						the source code of this site, and many more of my projects, on the 'Projects' page. If
+						you want to work with me or if you have some sort of suggestion or if you have some sort
+						of question, you can contact me from the 'Contact' page.
+					</p>
+				</div>
 			</div>
 		);
 	}
