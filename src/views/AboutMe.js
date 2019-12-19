bdbc047f32bf50f2c867ad903929314947825a5a
randomprogramming <isnipedodac@gmail.com>
2019-12-19 15:08:19 +0100

    turned the links on about me page into flexboxes

--- src/views/AboutMe.js
+++ src/views/AboutMe.js
@@ -13,8 +13,8 @@ export default class AboutMe extends Component {
 			// TODO: Rewrite this and turn a link into a component to reduce the repetitive code
 			<div className="about-me-container">
 				<div className="links-container">
-					<div className="link-name align-end">
-						<span className="link-name-text">{links.name.text}</span>
+					<div className="link align-end">
+						<div className="link-text">{links.name.text}</div>
 						<img
 							className="link-name-img link-img"
 							src={links.name.icon}
@@ -23,7 +23,7 @@ export default class AboutMe extends Component {
 						/>
 					</div>
 
-					<div className="link-location">
+					<div className="link">
 						<img
 							className="link-location-img link-img"
 							src={links.location.icon}
@@ -31,11 +31,11 @@ export default class AboutMe extends Component {
 							style={{ borderColor: "#1689FC" }}
 						/>
 
-						<span className="link-location-text">{links.location.text}</span>
+						<div className="link-text">{links.location.text}</div>
 					</div>
 
-					<div className="link-email align-end">
-						<span className="link-email-text">{links.email.text}</span>
+					<div className="link align-end">
+						<div className="link-text">{links.email.text}</div>
 						<img
 							className="link-email-img link-img"
 							src={links.email.icon}
@@ -44,22 +44,22 @@ export default class AboutMe extends Component {
 						/>
 					</div>
 
-					<div className="link-phone">
+					<div className="link">
 						<img
 							className="link-phone-img link-img"
 							src={links.phone.icon}
 							alt="phone"
 							style={{ borderColor: "#1689FC" }}
 						/>
-						<span className="link-phone-text">{links.phone.text}</span>
+						<div className="link-text">{links.phone.text}</div>
 					</div>
 
-					<div className="link-github align-end">
-						<span className="link-github-text">
+					<div className="link align-end">
+						<div className="link-text">
 							<a href="https://github.com/randomprogramming" style={{ color: "white" }}>
 								{links.github.text}
 							</a>
-						</span>
+						</div>
 						<img
 							className="link-github-img link-img"
 							src={links.github.icon}
